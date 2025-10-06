22b21086fb2e8e70350d9cc5d3a3417bb6941424
seniru jayaruwan <senirusennath73@gmail.com>
2025-10-06 15:07:18 +0530

    Done🚀

---
+++ web-project-01/src/pages/admin/productsAdminPage.jsx
@@ -0,0 +1,8 @@
+export default function ProductsAdminPage() {
+    return(
+        <div className="w-full h-full border-[3px]">
+            Testing Product admin Page
+        </div>
+    )
+    
+}
--- web-project-01/src/pages/adminPage.jsx
+++ web-project-01/src/pages/adminPage.jsx
@@ -3,22 +3,23 @@ import { FaBagShopping } from "react-icons/fa6";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaUserFriends } from "react-icons/fa";
 import { IoSettings } from "react-icons/io5";
+import ProductsAdminPage from "./admin/productsAdminPage";
 export default function AdminPage() {
     return(
-        <div className="w-full h-screen flex ">
-            <div className="w-[300px] h-full flex flex-col items-center"> 
+        <div className="w-full h-screen flex bg-gray-300">
+            <div className="w-[300px] h-full flex flex-col items-center bg-blue-300"> 
                 <span className="text-3xl font-bold my-5">Admin Page</span>
-            <Link className="flex flex-row h-[60px] w-full items-center text-xl text-red-500 gap-[7px] p-[20px]" to={"/admin/products"}><FaBagShopping/>Products</Link>
-            <Link className="flex flex-row h-[60px] w-full items-center text-xl text-red-500 gap-[7px] p-[20px]" to={"/admin/orders"}><FaShoppingCart />Orders</Link>
-            <Link className="flex flex-row h-[60px] w-full items-center text-xl text-red-500 gap-[7px] p-[20px]" to={"/admin/users"}><FaUserFriends />Users</Link>
-            <Link className="flex flex-row h-[60px] w-full items-center text-xl hover:border-2 text-red-500 gap-[7px] p-[20px]" to={"/admin/settings"}><IoSettings />Settings</Link>
+            <Link className="flex flex-row h-[60px] w-full items-center text-xl text-black-500 gap-[7px] p-[20px]  hover:bg-gray-700 hover:text-amber-50 rounded-l-3xl" to={"/admin/products"}><FaBagShopping/>Products</Link>
+            <Link className="flex flex-row h-[60px] w-full items-center text-xl text-black-500 gap-[7px] p-[20px]  hover:bg-gray-700 hover:text-amber-50 rounded-l-3xl" to={"/admin/orders"}><FaShoppingCart />Orders</Link>
+            <Link className="flex flex-row h-[60px] w-full items-center text-xl text-black-500 gap-[7px] p-[20px]  hover:bg-gray-700 hover:text-amber-50 rounded-l-3xl" to={"/admin/users"}><FaUserFriends />Users</Link>
+            <Link className="flex flex-row h-[60px] w-full items-center text-xl  text-black-500 gap-[7px] p-[20px] hover:bg-gray-700 hover:text-amber-50 rounded-l-3xl" to={"/admin/settings"}><IoSettings />Settings</Link>
 
             
             </div>
             <div className="w-[calc(100%-300px)] h-full">
                 <Routes path="/">
                     <Route path="/" element={<h1>Dashboard</h1>}/>
-                    <Route path="/products" element={<h1>Products</h1>}/>
+                    <Route path="/products" element={<ProductsAdminPage   />}/>
                     <Route path="/orders" element={<h1>Orders</h1>}/>
                 </Routes>
             </div>
